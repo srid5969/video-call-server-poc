25ecee1b066a2a8fa42f851ea77ba45988d52696
Sridhar <156986808+sridhar5969@users.noreply.github.com>
2024-10-26 20:52:46 +0530

    Refactor app module to use global configuration and add database name
    - Import appConfig from './config/app-config'
    - Add ConfigModule.forRoot() to import appConfig globally
    - Add dbName property to MongooseModule.forRootAsync() configuration
    
    Related to #issue-number

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,18 +8,25 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import appConfig from './config/app-config';
 
 @Module({
   imports: [
     AuthModule,
     UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+    }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGODB_URI'),
+        dbName: configService.get<string>('DATABASE'),
       }),
       inject: [ConfigService],
-    })  ],
+    }),
+  ],
   controllers: [AppController, VideoCallController],
   providers: [AppService, RoomService, VideoCallGateway],
 })
--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,4 +1,4 @@
-const appConfig = {
+const appConfig =()=>( {
   PORT: process.env.PORT || 3000,
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/nest',
   DATABASE: process.env.DATABASE || 'development',
@@ -11,5 +11,5 @@ const appConfig = {
   TURN_SERVER: process.env.TURN_SERVER || 'turn:turn.bistri.com:80',
   TURN_USERNAME: process.env.TURN_USERNAME || 'homeo',
   TURN_CREDENTIAL: process.env.TURN_CREDENTIAL || 'homeo',
-};
+});
 export default appConfig;
